0a8c1f3104d68b17417fd86000f3ca01a8abcb5f
Olga Voronenko <olga.voronenko@tptlive.ee>
2025-09-04 10:57:30 +0300

    math examples

--- index.php
+++ index.php
@@ -1,58 +1,24 @@
 <?php
 
-$array = array(1, 2, 3);
-var_dump($array);
-$array = [1, 2, 3];
-var_dump($array);
-$array = [
-    1,
-    'olga',
-    true ,
-    [1, 2, 3],
-    3.4,
-];
-var_dump($array);
-$array  = [
-    'name' => 'Olga',
-    'age' => '18',
-    'isFemale' => true,
-];
-var_dump($array);
-
-$array = [
-    100 => 1,
-    2,
-    'name' => 'Olga',
-    'age' => '18',
-    6,
-    'isFemale' => true,
-    3,
-    4,
-    5,
-];
-var_dump($array);
-
-$array = [1, 2, 3];
-
-var_dump($array[1]);
-$array = [
-    'name' => 'Olga',
-    'age' => '18',
-    'isFemale' => true,
-];
-var_dump($array['name']);
-
-$array = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-];
-var_dump($array[2][1]);
-$array = [1,2,3];
-var_dump($array);
-array_push($array, 4);
-array_push($array, 5,6,7);
-$array[] = 8;
-var_dump($array);
-$array[1] = 'something else';
-var_dump($array);
+$num = 1;
+$num = $num + 1;
+var_dump($num);
+$num = $num - 1;
+var_dump($num);
+$num = $num * 2;
+var_dump($num);
+$num = $num / 2;
+var_dump($num);
+$num = $num % 2;
+var_dump($num);
+$num +=1;
+$num -=1;
+$num /=2;
+$num *=2;
+$num %=2;
+var_dump($num);
+$var++;
+$var--;
+var_dump($num);
+$num = $num ** 2;
+$num **=2;

139281bc620aaeb959f83d3bfb9e8af9fe44fff3
Olga Voronenko <olga.voronenko@tptlive.ee>
2025-09-04 10:13:11 +0300

    hello world php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "Hello World\n";
+echo "Hello World\n";
+$name = 'Olga';
+echo 'Hello ' . $name;
+echo "\n";
+$age = 18;
+echo 'Your name is ' . $name . ' and you are ' . $age . ' yaers old!';
+echo "\n";
+
+$var = 1;
+var_dump($var);
+$var = 1.3;
+var_dump($var);
+$var = 'Hellõ';
+var_dump($var);
+$var = true;
+var_dump($var);
+$var = false;
+var_dump($var);
+$bar = null;
+var_dump($bar);
+
+?>

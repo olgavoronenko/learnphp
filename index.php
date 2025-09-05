d720dae53af8dbfa9e566dbfb2d537c914826a9e
Olga Voronenko <olga.voronenko@tptlive.ee>
2025-09-05 14:15:17 +0300

    add visibility example

--- index.php
+++ index.php
@@ -1,28 +1,37 @@
 <?php
 
 class Box {
-    public $height;
-    public $width;
-    public $length;
+    public $height = 0;
+    private $width = 0;
+    public $length = 0;
 
-    public function volume(){
+    public function __construct($height, $width, $lenght) {
+        $this->$height = $height;
+        $this->width = $width;
+        $this->length = $length;
+    }
+
+    public function setHeight($width){
+       if(is_numeric($width) && $width > 0){
+        $this->width = $width;
+       } else{
+        throw new Exception('Width needs to be a number and bigger than 0');
+       }
+    }
+
+        public function volume(){
         return $this->$height * $this->width * $this->length;
     }
 }
 
-$box1 = new Box();
-$box1->height = 10;
-$box1->width = 10;
-$box1->length = 10;
+$box1 = new Box(10, 10, 10);
 $volume = $box1->volume();
 var_dump($box1);
 var_dump($volume);
 var_dump($box1->volume());
 
-$box2 = new Box();
-$box2->height = 40;
-$box2->width = 60;
-$box2->length = 70;
+$box2 = new Box(40, 60, 70);
+
 var_dump($box2);
 var_dump($box2->volume());
 var_dump($box1);

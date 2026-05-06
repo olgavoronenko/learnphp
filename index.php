290494886a292b6ef6a95a9bf4da77db5fdded8d
Olga Voronenko <olga.voronenko@tptlive.ee>
2026-05-06 15:57:28 +0300

    Add innheritance example

--- index.php
+++ index.php
@@ -23,6 +23,17 @@ class Box {
         return $this->height * $this->width * $this->length;
     }
 }
+
+class MetalBox extends Box {
+    public $material = "Metal";
+    public $weightPerUnit = 2;
+
+    public function weight() {
+        return $this->volume() * $this->weightPerUnit;
+    }
+    
+}
+
 $box1 = new Box(10,10,10);
 $volume1 = $box1->volume();
 var_dump($box1);
@@ -32,3 +43,13 @@ $box2 = new Box(30,40,50);
 $volume2 = $box2->volume();
 var_dump($box2);
 var_dump($volume2);
+
+$metalBox = new MetalBox(10,20,40);
+var_dump($metalBox);
+var_dump($metalBox->volume());
+var_dump($metalBox->weight());
+
+class IronBox extends MetalBox {
+    public $material = "Iron";
+    public $weightPerUnit = 3;
+}

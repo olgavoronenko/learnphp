82a49a365743062d2639f10b21c2d8c28f85050f
Olga Voronenko <olga.voronenko@tptlive.ee>
2026-05-06 16:01:04 +0300

    Magic methods

--- index.php
+++ index.php
@@ -1,55 +1,99 @@
 <?php
 
 class Box {
-    public $height;
-    public $width;
-    public $length;
+    public $height = 0;
+    private $width = 0;
+    protected $length = 0;
 
-    public function __construct( $height, $width, $length ) {
+    public static $count = 0;
+
+    public static function cool(){
+        var_dump(self::$count); 
+        var_dump(self::class); 
+        var_dump(static::class); 
+    }
+
+    public function __construct($height=0, $width=0, $length=0) {
+        self::$count++;
         $this->height = $height;
         $this->width = $width;
         $this->length = $length;
     }
 
-  public function setWidth($width) {
-    if(is_numeric($width) && $width > 0) {
-      $this->width = $width;
-    } else {
-      throw new Exception("Width must be a positive number.");
+    public function test(){
+        var_dump($this->width);
     }
-  }
+    public function setWidth($width){
+        if(is_numeric($width) && $width > 0){
+            $this->width = $width;
+        } else {
+            throw new Exception('width needs to be number and bigger than 0');
+        }
+    }
 
     public function volume() {
         return $this->height * $this->width * $this->length;
     }
+
+    public function __toString() {
+        return 'Im a box!';
+    }
+    
+    public function __destruct()
+    {
+        var_dump('Object was destroyed!');
+    }
+
+    public function __get($name){
+        var_dump("Trying to get $name");
+        return 'Some cool value';
+    }
+
+    public function __set($name, $value){
+        var_dump("Trying to set $name with value $value");
+    }
+
+    public function __call($name, $args){
+        var_dump("Trying to call function $name with values", $args);
+    }
+
+    public static function __callStatic($name, $args){
+        var_dump("Trying to call function $name with values", $args);
+    }
+
+    public function __invoke(...$args){
+        var_dump('I was called as function with args', $args);
+    }
 }
 
 class MetalBox extends Box {
-    public $material = "Metal";
+    public $material = 'Metal🤘';
     public $weightPerUnit = 2;
 
-    public function weight() {
+    public function test(){
+        var_dump($this->length);
+    }
+
+    public function mass(){
         return $this->volume() * $this->weightPerUnit;
     }
-    
 }
-
-$box1 = new Box(10,10,10);
-$volume1 = $box1->volume();
-var_dump($box1);
-var_dump($volume1);
-
-$box2 = new Box(30,40,50);
-$volume2 = $box2->volume();
-var_dump($box2);
-var_dump($volume2);
-
-$metalBox = new MetalBox(10,20,40);
-var_dump($metalBox);
-var_dump($metalBox->volume());
-var_dump($metalBox->weight());
 
 class IronBox extends MetalBox {
-    public $material = "Iron";
+    public $material = 'Iron';
     public $weightPerUnit = 3;
 }
+
+function makeBox(){
+    $box1 = new Box();
+    var_dump($box1->potato);
+    var_dump($box1->asldlasdlashn);
+    $box1->kakajunn = 123;
+    $box1->hello(1, 2, 3);
+    $box1(1, 3, 4, 5);
+    echo $box1;
+}
+makeBox();
+
+$box1 = 1;
+var_dump('End of program');

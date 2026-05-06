53902dc2e3f0da92bbca0455458ff25904f760fa
Olga Voronenko <olga.voronenko@tptlive.ee>
2026-05-06 12:50:31 +0300

    Add math

--- index.php
+++ index.php
@@ -1,61 +1,24 @@
 <?php
-
-$array = array(1, 2, 3,);
-var_dump($array);
-$array = [1, 2, 3,];
-var_dump($array);
-$array = [
-    1,
-'Olga',
-true,
-[1, 2, 3,],
-3.4,
-];
-var_dump($array);
-$array = [
-    'name' => 'Olga',
-    'age' => 19,
-    'isFemale' => true,
-];
-var_dump($array);
-
-var_dump($array);
-$array = [
-    100 => 1,
-    2,
-    'name' => 'Olga',
-    'age' => 19,
-    'isFemale' => true,
-    3,
-    4,
-    5,
-];
-var_dump($array);
-
-$array = [1, 2, 3,];
-
-var_dump($array[1]);
-
- $array = [
-    'name' => 'Olga',
-    'age' => 19,
-    'isFemale' => true, ];
-
-var_dump($array['name']);
-
-$array = [ 
-    [1, 2, 3,],
-    [4, 5, 6,],
-    [7, 8, 9,],
-];
-var_dump($array[2][1]);
-
-$array = [1, 2, 3];
-var_dump($array);
-array_push($array, 4);
-array_push($array, 5, 6, 7);
-$array[] = 8;
-var_dump($array);
-
-$array[1]= 'something else';
-var_dump($array);
+$num = 1;
+$num = $num + 1;
+var_dump($num);
+$num = $num - 1;
+var_dump($num);
+$num = $num * 2;
+var_dump($num);
+$num = $num / 2;
+var_dump($num);
+$num = $num % 2;
+var_dump($num);
+$num += 1;
+$num -= 1;
+$num *= 2;
+$num /= 2;
+$num %= 2;
+var_dump($num);
+$num++;
+$num--;
+var_dump($num);    
+$num = $num **2;
+$num **=2;
+var_dump($num);

867c8c2cd163454fcd76f7e8d460fc51b50832f9
Olga Voronenko <olga.voronenko@tptlive.ee>
2026-05-06 13:46:29 +0300

    Construct

--- index.php
+++ index.php
@@ -5,23 +5,24 @@ class Box {
     public $width;
     public $length;
 
+    public function __construct( $height, $width, $length ) {
+        $this->height = $height;
+        $this->width = $width;
+        $this->length = $length;
+  
+    }
+
     public function volume() {
         return $this->height * $this->width * $this->length;
     }
 }
 
-$box1 = new Box();
-$box1->height = 10;
-$box1->width = 10;
-$box1->length = 10;
+$box1 = new Box(10,10,10);
 $volume1 = $box1->volume();
 var_dump($box1);
 var_dump($volume1);
 
-$box2 = new Box();
-$box2->height = 5;
-$box2->width = 5;   
-$box2->length = 5;
+$box2 = new Box(30,40,50);
 $volume2 = $box2->volume();
 var_dump($box2);
 var_dump($volume2);

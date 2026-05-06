555a3cb934b47074b4873ce92e85544068c85384
Olga Voronenko <olga.voronenko@tptlive.ee>
2026-05-06 12:48:20 +0300

    var example

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "Hello, World!\n"; 
+echo "Hello, World!\n";
+$name = "Olga";
+echo "Hello " . $name ;
+echo "\n";
+$age = 19;
+echo "Your name is $name and you are $age years old.";
+echo "\n";
+echo "Your name is " . $name . " and you are " . $age . " years old.";
+
+$var = 1;
+var_dump($var);
+$var = 1.5;
+var_dump($var);
+$var = "HellÕ";
+var_dump($var);
+$var = true;
+var_dump($var);
+$var = false;
+var_dump($var);
+$bar = null;
+var_dump($bar);
+?>

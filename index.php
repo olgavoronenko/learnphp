9d30824d5150d36c8b9f034d6ca0f49a4dd1c680
Olga Voronenko <olga.voronenko@tptlive.ee>
2026-05-06 12:53:20 +0300

    Add if and switch examples

--- index.php
+++ index.php
@@ -1,25 +1,56 @@
 <?php
+$num = 5;
+if ($num > 5) {
+    var_dump('Bigger than 5');
+} elseif ($num === 5) {
+    var_dump('Equal to 5');
+} else {
+    var_dump('Smaller than 5');             
+}
 
-$bool = true && false;
-var_dump($bool); 
+$day = (int)date('N', strtotime('06.05.2026'));
+var_dump($day);
+if ($day === 1) {
+    var_dump('Esmaspäev');
+} elseif ($day === 2) {
+    var_dump('Teisipäev');
+} elseif ($day === 3) {
+    var_dump('Kolmapäev');
+} elseif ($day === 4) {
+    var_dump('Neljapäev');
+} elseif ($day === 5) {
+    var_dump('Reede');
+} elseif ($day === 6) {
+    var_dump('Laupäev');
+} elseif ($day === 7) {
+    var_dump('Pühapäev');
+} else {
+    var_dump('Vigane päev');             
+}
 
-$bool = true || false;
-var_dump($bool);
+switch($day) {
+    case 1:
+        var_dump('Esmaspäev');
+        break;
+    case 2:
+        var_dump('Teisipäev');
+        break;
+    case 3:
+        var_dump('Kolmapäev');
+        break;
+    case 4:
+        var_dump('Neljapäev');
+        break;
+    case 5:
+        var_dump('Reede');
+        break;
+    case 6:
+        var_dump('Laupäev');
+        break;
+    case 7:
+        var_dump('Pühapäev');
+        break;       
+}
 
-$bool = !true;
-var_dump($bool);
 
-$bool = !false;
-var_dump($bool);
-
-$bool = 10 > 5;
-var_dump($bool);
-
-$bool = 10 == 10;
-var_dump($bool);
-
-$sbool = 10 != 10;
-$sbool = 10 !== 10;
-
-$bool = (true && false || true && !false) && true;
-var_dump($bool);
+    

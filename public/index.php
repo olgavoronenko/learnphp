e762e77310277a2c3c25354e668bc04bb363c132
Olga Voronenko <olga.voronenko@tptlive.ee>
2026-05-06 16:06:55 +0300

    Add simple router script

---
+++ public/index.php
@@ -0,0 +1,47 @@
+<?php
+
+
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
+    return false; // serve the requested resource as-is.
+}
+
+
+
+
+
+function dump(...$args) {
+     
+        echo '<pre>';
+        var_dump(...$args);
+        echo '</pre>';
+    
+}
+
+
+
+
+switch($_SERVER['REQUEST_URI']) {
+    case '/':
+        $title = 'World news';  
+        $posts = [
+    ['title' => 'World news 1', 'author' => 'Pets', 'published' => '12.09.2025', 'body' => 'Some world news 1 body'],
+    ['title' => 'World news 2', 'author' => 'Maali', 'published' => '11.09.2025', 'body' => 'Some world news 2 body'],
+    ['title' => 'World news 3', 'author' => 'Annika', 'published' => '10.09.2025', 'body' => 'Some world news 3 body'],
+    ['title' => 'World news 4', 'author' => 'Mats', 'published' => '09.09.2025', 'body' => 'Some world news 4 body'],
+  ];
+
+     include __DIR__ . '/../views/index.php';
+     break;
+    case '/us':
+        $title = 'U.S news';
+        $posts = [
+    ['title' => 'U.S news 1', 'author' => 'Pets', 'published' => '12.09.2025', 'body' => 'Some U.S news 1 body'],
+    ['title' => 'U.S news 2', 'author' => 'Maali', 'published' => '11.09.2025', 'body' => 'Some U.S news 2 body'],
+    ['title' => 'U.S news 3', 'author' => 'Annika', 'published' => '10.09.2025', 'body' => 'Some U.S news 3 body'],
+    ['title' => 'U.S news 4', 'author' => 'Mats', 'published' => '09.09.2025', 'body' => 'Some U.S news 4 body'],
+  ];
+    include __DIR__ .'/../views/us.php';
+    default:
+        echo "404 Not Found";
+        break;
+}
---
+++ views/us.php
@@ -0,0 +1,13 @@
+
+<?php include 'partials/header.php'; ?>
+    <main class="container">
+      <div class="row g-5">
+        <div class="col-md-8">
+          <?php include 'partials/posts.php'; ?>
+        </div>
+        <div class="col-md-4">
+          <?php include 'partials/sidebar.php'; ?>
+        </div>
+      </div>
+    </main>
+<?php include 'partials/footer.php'; ?>

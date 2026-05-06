ca6a4761e6fbabb807d1c8b8b715f8570367ae80
Olga Voronenko <olga.voronenko@tptlive.ee>
2026-05-06 13:41:40 +0300

    Add function example

--- index.php
+++ index.php
@@ -1,39 +1,32 @@
 <?php
 
-for ($i = 0; $i < 10; $i++) {
-    var_dump($i);
+function sayHello(){
+    var_dump("Hello");
 }
 
-for ($i=10 ; $i > 0; $i--) {
-    var_dump($i);
+sayHello();
+
+function sayHelloToMe($name = 'Nameless'){
+    var_dump("Hello $name!");
 }
 
-for ($i=1; $i < 1_000_000; $i*=2) {
-    var_dump($i);
+sayHelloToMe('John');
+sayHelloToMe('Jane');
+sayHelloToMe('Doe');
+sayHelloToMe();
+
+function sayNameAndAge($name, $age){
+    var_dump("Your name is $name and you are $age years old.");
 }
 
-$current = date('s');
+sayNameAndAge('Olga', 19);
+sayNameAndAge('Alice', 25);
 
-// while ($current < 59){
-//     var_dump($current);
-//     $current = (int)date('s');
-// }
-$i = 10;
-
-while ($i < 10){
-    var_dump('while');
+function recursion($i){
+    if($i < 10){
+        var_dump($i);
+        recursion($i + 1);
+    }
 }
 
-
-do{
-    var_dump('do while');
-} while( $i < 10);
-
-$array = [
-    'name' => Olga,
-    'age' => 19,
-    'isFemale' => true,
-];
-foreach ($array as $key => $value) {
-    var_dump($key, $value);
-}
+recursion(0);

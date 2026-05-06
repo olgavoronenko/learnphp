8abb394324cbd88d1ad1de263a8ed791823defa9
Olga Voronenko <olga.voronenko@tptlive.ee>
2026-05-06 12:49:47 +0300

    Add array examples

--- index.php
+++ index.php
@@ -1,25 +1,61 @@
 <?php
 
-echo "Hello, World!\n"; 
-echo "Hello, World!\n";
-$name = "Olga";
-echo "Hello " . $name ;
-echo "\n";
-$age = 19;
-echo "Your name is $name and you are $age years old.";
-echo "\n";
-echo "Your name is " . $name . " and you are " . $age . " years old.";
+$array = array(1, 2, 3,);
+var_dump($array);
+$array = [1, 2, 3,];
+var_dump($array);
+$array = [
+    1,
+'Olga',
+true,
+[1, 2, 3,],
+3.4,
+];
+var_dump($array);
+$array = [
+    'name' => 'Olga',
+    'age' => 19,
+    'isFemale' => true,
+];
+var_dump($array);
 
-$var = 1;
-var_dump($var);
-$var = 1.5;
-var_dump($var);
-$var = "HellÕ";
-var_dump($var);
-$var = true;
-var_dump($var);
-$var = false;
-var_dump($var);
-$bar = null;
-var_dump($bar);
-?>
+var_dump($array);
+$array = [
+    100 => 1,
+    2,
+    'name' => 'Olga',
+    'age' => 19,
+    'isFemale' => true,
+    3,
+    4,
+    5,
+];
+var_dump($array);
+
+$array = [1, 2, 3,];
+
+var_dump($array[1]);
+
+ $array = [
+    'name' => 'Olga',
+    'age' => 19,
+    'isFemale' => true, ];
+
+var_dump($array['name']);
+
+$array = [ 
+    [1, 2, 3,],
+    [4, 5, 6,],
+    [7, 8, 9,],
+];
+var_dump($array[2][1]);
+
+$array = [1, 2, 3];
+var_dump($array);
+array_push($array, 4);
+array_push($array, 5, 6, 7);
+$array[] = 8;
+var_dump($array);
+
+$array[1]= 'something else';
+var_dump($array);
